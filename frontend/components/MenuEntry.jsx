10f9dadd53940c6878c95e394ad467958ac45f51
O.Bilenko <oleksandr.bilenko@shopgate.com>
2020-12-17 10:24:40 +0100

    CCP-2346 small adaptation for extension @shopgate/cms

--- frontend/components/MenuEntry.jsx
+++ frontend/components/MenuEntry.jsx
@@ -1,17 +1,20 @@
-import { NAV_MENU_TERMS, NAV_MENU_PRIVACY, NAV_MENU_IMPRINT } from '@shopgate/pwa-common/constants/Portals';
-import { NAV_MENU_SHIPPING, NAV_MENU_PAYMENT, NAV_MENU_RETURN_POLICY } from '@shopgate/pwa-common-commerce/market/constants/Portals';
-import config from './../config';
+import {
+  NAV_MENU_TERMS, NAV_MENU_PRIVACY, NAV_MENU_IMPRINT, NAV_MENU_STORE_INFORMATION,
+} from '@shopgate/engage/core';
+import { NAV_MENU_SHIPPING, NAV_MENU_PAYMENT, NAV_MENU_RETURN_POLICY } from '@shopgate/engage/market';
+import config from '../config';
+
+const { navMenu = {} } = config;
+const allOff = Object.values(navMenu).every(value => !value);
 
 const {
-  navMenu: {
-    shipping = true,
-    payment = true,
-    terms = true,
-    privacy = true,
-    'return-policy': returnPolicy = true,
-    imprint = true,
-  } = {},
-} = config;
+  shipping = true,
+  payment = true,
+  terms = true,
+  privacy = true,
+  'return-policy': returnPolicy = true,
+  imprint = true,
+} = navMenu;
 
 /**
  * Check if config is on for given portal
@@ -26,6 +29,7 @@ const isMenuActive = (name) => {
     case NAV_MENU_PRIVACY: return privacy;
     case NAV_MENU_RETURN_POLICY: return returnPolicy;
     case NAV_MENU_IMPRINT: return imprint;
+    case NAV_MENU_STORE_INFORMATION: return !allOff;
     default: return true;
   }
 };
